fc448e8db26184b522887d3d11face8e4b8f5eb4
Marian Noekel <mariannoekel@gmail.com>
2026-08-14 16:19:09 +0200

    test darkmode samsung browser

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -47,46 +47,18 @@ export default function Navbar({ className }: Props) {
             Psychotherapie Praxis Anne <br /> Julia Röhl
           </Link>
           <Link href={"#"}>test2</Link>
-          <Link
-            href="#"
-            className="burgermenu"
+          <div
+            className={`burgermenu ${showMenu ? 'open' : ''}`}
             id="burger-menu"
             onClick={toggleMenu}
             aria-label="Menü öffnen"
           >
-            <svg
-              width="40"
-              height="30"
-              viewBox="0 0 40 30"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <rect
-                className="top"
-                y="0"
-                width="40"
-                height="3"
-                rx="1.5"
-                fill="currentColor"
-              />
-              <rect
-                className="middle"
-                y="10"
-                width="40"
-                height="3"
-                rx="1.5"
-                fill="currentColor"
-              />
-              <rect
-                className="bottom"
-                y="20"
-                width="40"
-                height="3"
-                rx="1.5"
-                fill="currentColor"
-              />
+            <svg className="" viewBox="0 0 40 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <rect className="top" width="40" height="3" rx="1.5" fill="currentColor" />
+                <rect className="middle" y="10" width="40" height="3" rx="1.5" fill="currentColor" />
+                <rect className="bottom" y="20" width="40" height="3" rx="1.5" fill="currentColor" />
             </svg>
-          </Link>
+          </div>
         </div>
         {showMenu ? (
           <div>
